75ce0655c8ffb4a6b4275f3a2ac7e7ca0ba4e436
ahjiuhdotcom <ahjiuhdotcom@yahoo.com>
2016-09-20 22:38:31 +0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: "Meet mom for lunch",
-	completed: false
-}, {
-	id: 2,
-	description: "Go to market",
-	completed: false
-}, {
-	id: 3,
-	description: "Web dev homework",
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+// var todos = [{
+// 	id: 1,
+// 	description: "Meet Jiji for lunch",
+// 	completed: false
+// }, {
+// 	id: 2,
+// 	description: "Go to market",
+// 	completed: false
+// }, {
+// 	id: 3,
+// 	description: "Web dev homework",
+// 	completed: true
+// }];
 
 app.get("/", function(req, res){
 	res.send("To do API Root");
@@ -32,10 +38,25 @@ app.get("/todos/:id", function(req, res){
 	for (var i=0; i<todos.length; i++) {
 		var todo = todos[i];
 		if (todo.id == todoId) {
+			// if using (todo.id === todoId), need to change todoId to js number (todo.id is number)
+			// using parseInt(re.params.id, 10) (most of the time using 10, type of number format)
 			return res.json(todos[i]);
 		} 
 	}
 	res.status(404).send();
+});
+
+// POST "/todos"
+// "body-parser" start required here
+app.post("/todos", function(req, res){
+	var body = req.body;
+	body.id = todoNextId;
+	todoNextId++;
+
+	todos.push(body);
+	
+	res.json(body);
+
 });
 
 
