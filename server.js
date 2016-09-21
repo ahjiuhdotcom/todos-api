d8c134c0f621a1125144833d17bbcdb943244cf5
ahjiuhdotcom <ahjiuhdotcom@yahoo.com>
2016-09-21 23:40:31 +0800

    Add ?completed filter

--- server.js
+++ server.js
@@ -28,13 +28,24 @@ app.get("/", function(req, res){
 
 // GET "/todos"
 app.get("/todos", function(req, res){
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	// "where" can return a array of matching item
+	if (queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
 	// this get request will return array of todos in json format
-	res.json(todos);
+	res.json(filteredTodos);
 });
 
 // GET "/todos/:id"
 app.get("/todos/:id", function(req, res){
 	var todoId =  parseInt(req.params.id, 10);
+	// "findWhere" only return one and first item
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
