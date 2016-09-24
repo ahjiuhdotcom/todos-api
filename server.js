038be8ff2d71c2ebd4679e0072b8034d127ac496
ahjiuhdotcom <ahjiuhdotcom@yahoo.com>
2016-09-24 16:08:45 +0800

    Convert GET/todos to sequelize

--- server.js
+++ server.js
@@ -31,26 +31,53 @@ app.get("/", function(req, res){
 // GET "/todos"
 app.get("/todos", function(req, res){
 
+	// // Filtering using "?xxx=zzz"
+	// var queryParams = req.query;
+	// var filteredTodos = todos;
+	// // "where" can return a array of matching item
+	// if (queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
+	// 	filteredTodos = _.where(filteredTodos, {completed: true});
+	// } else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
+	// 	filteredTodos = _.where(filteredTodos, {completed: false});
+	// }
 
-	// Filtering using "?xxx=zzz"
-	var queryParams = req.query;
-	var filteredTodos = todos;
-	// "where" can return a array of matching item
-	if (queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
-		filteredTodos = _.where(filteredTodos, {completed: false});
+	// if (queryParams.hasOwnProperty("q") && queryParams.q.length > 0){
+	// 	filteredTodos = _.filter(filteredTodos, function(todo){
+	// 		// "todo" is individual todo item in the array of filteredTodos
+	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+	// 	});
+	// }
+
+	// // this get request will return array of todos in json format
+	// res.json(filteredTodos);
+
+	var query = req.query;
+	var where = {};
+
+	if (query.hasOwnProperty("completed") && query.completed === "true"){
+		where.completed = true;			
+	} else if (query.hasOwnProperty("completed") && query.completed === "false"){
+		where.completed = true;		
 	}
 
-	if (queryParams.hasOwnProperty("q") && queryParams.q.length > 0){
-		filteredTodos = _.filter(filteredTodos, function(todo){
-			// "todo" is individual todo item in the array of filteredTodos
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+	if (query.hasOwnProperty("q") && query.q.length > 0){
+		where.description = {
+			$like: "%" + query.q + "%"
+		};
+	}
+
+	db.todo.findAll({where: where}).then(function(todos){
+			res.json(todos);
+			// if (!!todo) {
+			// 	res.json(todo.toJSON());
+			// } else {
+			// 	res.status(404).send();
+			// }
+		}).catch(function(e){
+			// "500" is server error
+			res.status(500).send();
 		});
-	}
 
-	// this get request will return array of todos in json format
-	res.json(filteredTodos);
 });
 
 // GET "/todos/:id"
